9a97b45c29b03708e58475d7ec110ca1a1983500
Alessio Magro <alessio@iolabs.ai>
2023-05-08 15:38:10 +0200

    Fixed gunicorn not finding an instance

--- backend/app.py
+++ backend/app.py
@@ -38,6 +38,8 @@ def create_app(*args, **kwargs):
     return app
 
 
+application = create_app()
+
+
 if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=5000, use_reloader=False)
+    application.run(host="0.0.0.0", port=5000, use_reloader=False)
